149c4ddc9dbee1b654e9f723424db1a846dfd2c0
Yuriy Poret <yuriy.poret@gmail.com>
2023-12-15 23:53:12 +0300

    Add Python code (lesson-23).

---
+++ half-year-2/python/lesson-23/helicopter.py
@@ -0,0 +1,53 @@
+from utils import randcell
+import os
+
+
+class Helicopter:
+    def __init__(self, w, h):
+        rc = randcell(w, h)
+        rx, ry = rc[0], rc[1]
+        self.w = w
+        self.h = h
+        self.x = rx
+        self.y = ry
+        self.tank = 0
+        self.tank_mx = 1
+        self.score = 0
+        self.lives = 20
+
+    def move(self, dx, dy):
+        nx, ny = dx + self.x, dy + self.y
+        if (nx >= 0 and ny >= 0 and nx < self.h and ny < self.w):
+            self.x, self.y = nx, ny
+
+    def print_stats(self):
+        print('💧 ', self.tank, '/', self.tank_mx, sep='', end=' | ')
+        print('🏆', self.score, end=' | ')
+        print('❤️ ', self.lives)
+
+    def game_over(self):
+        os.system("cls") # clear
+        print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        print("                                   ")
+        print("   GAME OVER, YOUR SCORE IS", self.score)
+        print("                                   ")
+        print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        exit(0)
+
+    def export_data(self):
+        return {
+            "score": self.score,
+            "lives": self.lives,
+            "x": self.x,
+            "y": self.y,
+            "tank": self.tank,
+            "tank_mx": self.tank_mx
+        }
+
+    def import_data(self, data):
+        self.x = data['x'] or 0
+        self.y = data['y'] or 0
+        self.tank = data['tank'] or 0
+        self.tank_mx = data['tank_mx'] or 1
+        self.lives = data['lives'] or 3
+        self.score = data['score'] or 0
---
+++ half-year-2/python/lesson-23/map.py
@@ -0,0 +1,130 @@
+from utils import randbool
+from utils import randcell
+from utils import randcell2
+
+
+# 0 - поле
+# 1 - дерево
+# 2 - река
+# 3 - госпиталь
+# 4 - апгрейд-шоп
+# 5 - огонь
+
+# '🟩🌲🌊🏥🏪'
+CELL_TYPES = ['🟩', '🌲 ', '🌊 ', '🏥 ', '🏪 ', '🔥 ']
+
+TREE_BONUS = 100
+UPGRADE_COST = 300 # TODO: 5000
+LIFE_COST = 500 # TODO: 10000
+
+class Map:
+
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.cells = [[0 for i in range(w)] for j in range(h)]
+        self.generate_forest(3, 10)
+        self.generate_river(10)
+        self.generate_river(10)
+        self.generate_upgrade_shop()
+        self.generate_hospital()
+
+    def check_bounds(self, x, y):
+        return not (x < 0 or y < 0 or x >= self.h or y >= self.w)
+
+    def print_map(self, helico, clouds):
+        print('⬛️ ' * (self.w + 2))
+        for ri in range(self.h):
+            print('⬛️ ', end='')
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if (clouds.cells[ri][ci] == 1):
+                    print('⬜️ ', end="")
+                elif (clouds.cells[ri][ci] == 2):
+                    print('🟥', end="")
+                elif (helico.x == ri and helico.y == ci):
+                    print("🚁 ", end="")
+                elif (cell >= 0 and cell < len(CELL_TYPES)):
+                    print(CELL_TYPES[cell], end='')
+            print('⬛️ ')
+        print('⬛️ ' * (self.w + 2))
+
+    def generate_river(self, l):
+        rc = randcell(self.w, self.h)
+        rx, ry = rc[0], rc[1]
+        self.cells[rx][ry] = 2
+        while l > 0:
+            rc2 = randcell2(rx, ry)
+            rx2, ry2 = rc2[0], rc2[1]
+            if (self.check_bounds(rx2, ry2)):
+                self.cells[rx2][ry2] = 2
+                rx, ry = rx2, ry2
+                l -= 1
+
+    def generate_forest(self, r, mxr):
+        for ri in range(self.h):
+            for ci in range(self.w):
+                if randbool(r, mxr):
+                    self.cells[ri][ci] = 1
+
+    def generate_tree(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        if (self.cells[cx][cy] == 0):
+            self.cells[cx][cy] = 1
+
+    def generate_upgrade_shop(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        self.cells[cx][cy] = 4
+
+    def generate_hospital(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        if self.cells[cx][cy] != 4:
+            self.cells[cx][cy] = 3
+        else:
+            self.generate_hospital()
+
+    def generate_fire(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        if self.cells[cx][cy] == 1:
+            self.cells[cx][cy] = 5
+
+    def update_fires(self):
+        for ri in range(self.h):
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if cell == 5:
+                    self.cells[ri][ci] = 0
+        for i in range(7):
+            self.generate_fire()
+
+    def process_helicopter(self, helico, clouds):
+        c = self.cells[helico.x][helico.y]
+        d = clouds.cells[helico.x][helico.y]
+        if (c == 2):
+            helico.tank = helico.tank_mx
+        if (c == 5 and helico.tank > 0):
+            helico.tank -= 1
+            helico.score += TREE_BONUS
+            self.cells[helico.x][helico.y] = 1
+        if (c == 4 and helico.score >= UPGRADE_COST):
+            helico.tank_mx += 1
+            helico.score -= UPGRADE_COST
+        if (c == 3 and helico.score >= LIFE_COST):
+            helico.lives += 10
+            helico.score -= LIFE_COST
+        if (d == 2):
+            helico.lives -= 1
+            if (helico.lives == 0):
+                helico.game_over()
+
+    def export_data(self):
+        return {
+            "cells": self.cells
+        }
+
+    def import_data(self, data):
+        self.cells = data['cells'] or [[0 for i in range(self.w)] for j in range(self.h)]
